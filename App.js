2127765de52fab8d35e6c1bc9163b170579c9c8a
rushabh1191 <rushabhb@gsteckno.com>
2018-05-23 00:12:17 +0530

    initial setup

--- App.js
+++ App.js
@@ -4,55 +4,83 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView
 } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
+type
+Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <ScrollView
+                horizontal={true}
+                pagingEnabled={true}
+                style={styles.pageStyle2}
+            >
+                <View style={styles.pageStyle} key="1">
+                    <Text>First page</Text>
+                </View>
+
+                <View style={styles.pageStyle} key="1">
+                    <Text>First page</Text>
+                </View><View style={styles.pageStyle} key="1">
+                <Text>First page</Text>
+            </View><View style={styles.pageStyle} key="1">
+                <Text>First page</Text>
+            </View>
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    viewPager: {
+        flex: 1,
+        padding: 10
+    },
+    pageStyle: {
+        backgroundColor: "#ffffff",
+        flex:1,
+        width:400
+    }, pageStyle2: {
+    }, pageStyle3: {
+        backgroundColor: "#E3e43f",
+        alignItems: 'center',
+        position: "absolute",
+        left: 0,
+        right: 0,
+        bottom: 0,
+        top: 0
+    }
+
 });
